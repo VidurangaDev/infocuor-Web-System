b763a5bfdac4171575b296f5f037634b6320a0f2
Amayuru Viduranga <100108274+VidurangaDev@users.noreply.github.com>
2024-10-10 12:54:42 +0530

    Components Structured
    get boostrap and setting components structure for cording environment

---
+++ infocuor_web/resources/views/pages/home.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HOME</title>
+    @include('libraries.boostrapstyle')
+  </head>
+  <body>
+    @include('components.navbar')
+
+    
+    @include('libraries.boostrapscript')
+  </body>
+</html>
