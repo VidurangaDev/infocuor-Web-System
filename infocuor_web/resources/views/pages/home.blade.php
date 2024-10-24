643cb6b9383a10dd752029a817d8a791aa3a08c6
Ruwani jayamanna <jayamannaruwani99@gmail.com>
2024-10-25 00:12:33 +0530

    home page modify

--- infocuor_web/resources/views/pages/home.blade.php
+++ infocuor_web/resources/views/pages/home.blade.php
@@ -128,7 +128,7 @@
         }
 
         .event-card:hover {
-            transform: scale(1.05);
+            transform: scale(1.15);
         }
 
         .event-card img {
@@ -201,7 +201,7 @@
             background-color: #022F56;
             color: white;
             text-align: center;
-            padding: 40px 0;
+            padding: 18px 0;
           
         }
 
@@ -242,7 +242,7 @@
 
 .footer-stats div:hover svg {
     fill: #007bff; /* Change icon color on hover */
-    transform: scale(1.1); /* Slightly enlarge icon on hover */
+    transform: scale(1.25); /* Slightly enlarge icon on hover */
 }
 
 .footer-stats div:hover p {
@@ -512,7 +512,65 @@ input[type="radio"] {
     75%, 100% { background-color: #fff; }
    
 }
+.review-section {
+    text-align: center;
+    padding: 50px;
+    background-color: white;
+   
+}
 
+.review-section h1 {
+    color: #2b3a4a;
+    margin-bottom: 20px;
+}
+        .reviews-container {
+            width: 60%;
+            margin: auto;
+            background-color: white;
+            border-radius: 10px;
+           
+            padding: 20px;
+        }
+
+        .review {
+            display: none;
+            font-size: 1.2em;
+        }
+
+        .review.active {
+            display: block;
+        }
+
+        .reviewer {
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .pagination {
+            margin-top: 10px;
+            align-items: center;
+            display: flex;
+    justify-content: center;
+   
+ }
+
+        .dot {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 15px;
+            width: 15px;
+            margin: 0 5px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+            
+        }
+
+        .dot.active {
+            background-color: #333;
+        }
 
     </style>
 
@@ -636,17 +694,17 @@ input[type="radio"] {
         <!-- Social Media Icons -->
         
         <div class="footer-media">
-        <a href="https://www.facebook.com/yourpage" target="_blank" class="social-icon mx-2">
+        <a href="https://www.facebook.com/infocuor" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg>
                 </a>
-                <a href="https://www.instagram.com/yourprofile" target="_blank" class="social-icon mx-2">
+                <a href="https://www.instagram.com/infocuor_lk?igsh=MXdjNjk0Yjg1anE4ZQ==" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
                     </svg>
                 </a>
-                <a href="https://www.linkedin.com/in/yourprofile" target="_blank" class="social-icon mx-2">
+                <a href="https://www.facebook.com/infocuor" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.196h.03V6.17h-2.4c.03.615 0 7.225 0 7.225h2.4z"/>
                     </svg>
@@ -682,17 +740,17 @@ input[type="radio"] {
         
         <div class="footer-media">
     
-                <a href="https://www.facebook.com/yourpage" target="_blank" class="social-icon mx-2">
+                <a href="https://www.facebook.com/infocuor" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg>
                 </a>
-                <a href="https://www.instagram.com/yourprofile" target="_blank" class="social-icon mx-2">
+                <a href="https://www.instagram.com/infocuor_lk?igsh=MXdjNjk0Yjg1anE4ZQ==" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
                     </svg>
                 </a>
-                <a href="https://www.linkedin.com/in/yourprofile" target="_blank" class="social-icon mx-2">
+                <a href="https://www.facebook.com/infocuor" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.196h.03V6.17h-2.4c.03.615 0 7.225 0 7.225h2.4z"/>
                     </svg>
@@ -729,17 +787,17 @@ input[type="radio"] {
         <!-- Social Media Icons -->
         
         <div class="footer-media">
-        <a href="https://www.facebook.com/yourpage" target="_blank" class="social-icon mx-2">
+        <a href="https://www.facebook.com/infocuor" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg>
                 </a>
-                <a href="https://www.instagram.com/yourprofile" target="_blank" class="social-icon mx-2">
+                <a href="https://www.instagram.com/infocuor_lk?igsh=MXdjNjk0Yjg1anE4ZQ==" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
                         <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
                     </svg>
                 </a>
-                <a href="https://www.linkedin.com/in/yourprofile" target="_blank" class="social-icon mx-2">
+                <a href="https://www.facebook.com/infocuor" target="_blank" class="social-icon mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.196h.03V6.17h-2.4c.03.615 0 7.225 0 7.225h2.4z"/>
                     </svg>
@@ -822,23 +880,62 @@ of University Students.</p>
 
 <section>
 <div class="review-section">
-        <h1>Reviews For Infocour</h1>
-        <div class="review-card">
-            <p class="name">Amayuru Viduranga</p>
-            <p class="review-text">Thank You infocuor for  Coverage of throughtout inter Uni Games. </p><p></p>Wish Your All the Best</p>
-            <div class="quote-icon">❞</div>
-            
-            
-            <div class="dots">
-                <span class="dot"></span>
-                <span class="dot active"></span>
-                <span class="dot"></span>
-            </div>
+<h1>Reviews For Infocour</h1>
+
+    <div class="reviews-container">
+        <!-- Slide 1 -->
+        <div class="review active">
+            <p class="reviewer">Amayuru Viduranga</p>
+            <p>Thank You infocuor for Coverage of throughout inter Uni Games.<br>
+               Wish You All the Best</p>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="review">
+            <p class="reviewer">Kasun Silva</p>
+            <p>Excellent work by Infocuor at the last university convocation. Great photography and videography!</p>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="review">
+            <p class="reviewer">Ruwan Perera</p>
+            <p>Thank you Infocuor for capturing the best moments during SLUG! Great job!</p>
+        </div>
+
+        <!-- Pagination dots -->
+        <div class="pagination">
+            <span class="dot active"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
         </div>
     </div>
+</div>
+
 </section>
 
+<script>
+     // JavaScript to enable auto-slide
+     let slideIndex = 0;
+        const slides = document.querySelectorAll(".review");
+        const dots = document.querySelectorAll(".dot");
 
+        function showSlides() {
+            slides.forEach((slide, index) => {
+                slide.style.display = "none";
+                dots[index].classList.remove("active");
+            });
 
+            slideIndex++;
+            if (slideIndex > slides.length) { 
+                slideIndex = 1; 
+            }
+
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].classList.add("active");
+            setTimeout(showSlides, 3000); // Change every 3 seconds
+        }
+
+        showSlides(); // Start the slide show
+</script>
 
     @endsection
